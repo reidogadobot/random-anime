e4e120517b4dd9e474a8a5531c35db5e5a1f8758
reidogadobot <79222246+reidogadobot@users.noreply.github.com>
2022-01-11 09:10:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const random = require("./src/random");
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 app.get("/anime", (req, res) => {
   res.type("json");
